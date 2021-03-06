7a10c6a647ee834b678c62d608acdfe5ce06c62e
Alexandru Birtoiu <alexandru.birtoiu20@imperial.ac.uk>
2021-03-06 15:23:56 +0200

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleOP.java
@@ -1,4 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FirstTeleOP {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "FirstTeleOP", group = "Linear Mode")
+public class FirstTeleOP extends LinearOpMode {
+    private Robot robot;
+
+    @Override
+    public void runOpMode() {
+        robot = new Robot(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            double xValue = gamepad1.left_stick_x;
+            double yValue = -gamepad1.left_stick_y;
+
+            robot.setMotorPowers(yValue + xValue,yValue - xValue,
+                    yValue + xValue,yValue- xValue);
+
+            telemetry.addData("xValue: ", xValue);
+            telemetry.addData("yValue: ", yValue);
+            telemetry.update();
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Hardware;
+
 public class Robot {
+    private final DcMotor frontRight;
+    private final DcMotor frontLeft;
+    private final DcMotor backRight;
+    private final DcMotor backLeft;
+
+    public Robot(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class,"fst");
+        frontRight = hardwareMap.get(DcMotor.class,"fdr");
+        backLeft = hardwareMap.get(DcMotor.class,"sst");
+        backRight = hardwareMap.get(DcMotor.class,"sdr");
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    public void setMotorPowers(double frontLeftPower, double frontRightPower,
+                               double backLeftPower, double backRightPower) {
+        frontRight.setPower(frontRightPower);
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+    }
+
+    public void setPowerForOneMotor(int motor, double power) {
+
+    }
 }
